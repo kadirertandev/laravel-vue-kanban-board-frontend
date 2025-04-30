ea4b042bbe9481021e22f3a9fe60d556309ac09c
Kadir Ertan <kadirertan000@gmail.com>
2025-04-30 14:48:36 +0300

    add login page using flowbite blocks

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1 class="text-3xl text-red-400 italic">hello world</h1>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink :to="{ name: 'login' }">Login</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "@/views/auth/LoginView.vue";
+import AuthLayout from "@/layouts/AuthLayout.vue";
+import BaseLayout from "@/layouts/BaseLayout.vue";
+import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "baseLayout",
+      component: BaseLayout,
+      children: [
+        {
+          path: "/",
+          name: "home",
+          component: HomeView,
+        },
+        {
+          path: "/about",
+          name: "about",
+          component: AboutView,
+        },
+      ],
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/",
+      name: "authLayout",
+      component: AuthLayout,
+      children: [
+        {
+          path: "/login",
+          name: "login",
+          component: LoginView,
+        },
+      ],
     },
   ],
 });
